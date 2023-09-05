db4c167989076412b5565e72a0b867eacbe36c27
Frank-Duffy <fduffy0328@gmail.com>
2023-09-05 16:56:08 -0400

    updated dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-from twodim_hist import __version__
+from feda_tools.__init__ import __version__
 
 setup(
-    name='twodim_hist',
+    name='feda_tools',
     version=__version__,
 
     url='https://github.com/SMB-Lab/feda_tools',
     author='Frank Duffy',
     author_email='fduffy0328@gmail.com',
 
-    py_modules=['twodim_hist'],
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        
+    ]
 )
