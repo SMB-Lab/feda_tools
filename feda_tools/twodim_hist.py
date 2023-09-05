98299fab65d94c610dad78cc938138b4ce68aa11
Frank-Duffy <fduffy0328@gmail.com>
2023-09-05 17:32:15 -0400

    add pytest support

--- feda_tools/twodim_hist.py
+++ feda_tools/twodim_hist.py
@@ -1,2 +1,11 @@
 def hello_world():
     print("This is my first pip package!")
+
+def cmd_args(args=None):
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('x', type=float)
+    parser.add_argument('y', type=float)
+    parsed_args = parser.parse_args(args)
+
+    print((parsed_args.x, parsed_args.y))
--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 from feda_tools.__init__ import __version__
 
+extra_dev = [
+        'pytest>=4',
+        'pytest-cov>=2',
+    ]
+
 setup(
     name='feda_tools',
     version=__version__,
@@ -10,11 +15,18 @@ setup(
     author='Frank Duffy',
     author_email='fduffy0328@gmail.com',
 
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=[
         'numpy',
         'pandas',
         'matplotlib',
-        
-    ]
+    ],
+    extras_require= {
+        'dev': extra_dev,
+    },
+    entry_points={
+    'console_scripts': [
+        'cmd=feda_tools.twodim_hist:cmd_args',
+        ],
+    },
 )
