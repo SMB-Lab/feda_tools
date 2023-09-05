d5481f2737073ce274a743371d885353da93abd9
Frank-Duffy <fduffy0328@gmail.com>
2023-09-05 14:07:34 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from twodim_hist import __version__
+
+setup(
+    name='twodim_hist',
+    version=__version__,
+
+    url='https://github.com/SMB-Lab/feda_tools',
+    author='Frank Duffy',
+    author_email='fduffy0328@gmail.com',
+
+    py_modules=['twodim_hist'],
+)
