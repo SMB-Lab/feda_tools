209b313df058307bef5728feab0e31739a119315
Frank-Duffy <fduffy0328@gmail.com>
2023-09-07 11:24:24 -0400

    added get_plot_dict and assoc. test

--- feda_tools/twodim_hist.py
+++ feda_tools/twodim_hist.py
@@ -13,30 +13,33 @@ calc_list = [
     "S(prompt)/S(total)"
 ]
 
+def get_plot_dict(yaml_file):
+    """
+    gets the plots specified by the user in the provided yaml file.
+    """
+    try:
+        # Conversts yaml doc to python object
+        plot_dict = yaml.safe_load(yaml_file)
+    except yaml.YAMLError as e:
+        print(e)
+    return plot_dict
+
 def make_2dhist(args=None):
     
     parser = argparse.ArgumentParser()
     parser.add_argument('data_folder', type=str)
-    parser.add_argument('config_file', type=str)
+    parser.add_argument('plot_file', type=str)
     parsed_args = parser.parse_args(args)
-
-    print((parsed_args.data_folder, parsed_args.config_file))
+    print((parsed_args.data_folder, parsed_args.plot_file))
     
-    with open(parsed_args.config_file) as stream:
-        try:
-            # Conversts yaml doc to python object
-            plot_batch = yaml.safe_load(stream)
-        
-            #Print the dict
-            print(d)
-        except yaml.YAMLError as e:
-            print(e)
+    with open(parsed_args.plot_file) as yaml_file:
+        plot_dict = get_plot_dict(yaml_file)
     
-    for plot in plot_batch:
-        xlabel = plot_batch[plot]['xlabel']
-        xfolder = plot_batch[plot]['xfolder']
-        ylabel = plot_batch[plot]['ylabel']
-        yfolder = plot_batch[plot]['yfolder']
+    for plot in plot_dict:
+        xlabel = plot_dict[plot]['xlabel']
+        xfolder = plot_dict[plot]['xfolder']
+        ylabel = plot_dict[plot]['ylabel']
+        yfolder = plot_dict[plot]['yfolder']
         
 
  
--- tests/test_twodim_hist.py
+++ tests/test_twodim_hist.py
@@ -8,3 +8,31 @@ def test_calc_list_entries():
     assert "Sg/Sr (prompt)" in calc_list
     assert "S(prompt)/S(total)" in calc_list
 
+def test_get_plot_dict(monkeypatch):
+    """
+    test to ensure that the get_plot_dict method returns a dict with the 
+    proper format.
+    """
+    from feda_tools.twodim_hist import get_plot_dict
+    import yaml
+
+
+    def mock_safe_load(stream):
+        mock_dict = {
+            'plot1': 
+            {
+                'xlabel': 'Number of Photons', 
+                'xfolder': 'bi4_bur', 
+                'ylabel': 'Duration (ms)', 
+                'yfolder': 'bi4_bur'
+            }
+        }
+        return mock_dict
+        
+
+    monkeypatch.setattr(yaml, "safe_load", mock_safe_load)
+
+    mock_plot_file = "plot.yaml"
+    plot_dict = get_plot_dict(mock_plot_file)
+    assert plot_dict == mock_safe_load(mock_plot_file)
+    
