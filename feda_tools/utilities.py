cc52416747642eaf438674289e51814dea529541
Frank-Duffy <fduffy0328@gmail.com>
2023-12-19 17:31:25 -0500

    added file_type parameter to read_analysis and cleaned up PTU notebook

--- feda_tools/utilities.py
+++ feda_tools/utilities.py
@@ -13,29 +13,26 @@ import tttrlib
 import os
 import tqdm
 
-def test():
-    print("TEST")
-
 def update_tttr_dict(
     df: pd.DataFrame,
     data_path: pathlib.Path,
-    tttrs: Dict[str, tttrlib.TTTR] = dict()    
+    tttrs: Dict[str, tttrlib.TTTR] = dict(),
+    file_type: str = "PTU"
 ):
     for ff, fl in zip(df['First File'], df['Last File']):
         try:
             tttr = tttrs[ff]
         except KeyError:
             fn = str(data_path / ff)
-            #tttr = tttrlib.TTTR(fn, 'HT3')
-            tttr = tttrlib.TTTR(fn, 'PTU')
-            #tttr = tttrlib.TTTR(fn, 'SPC-130')
+            tttr = tttrlib.TTTR(fn, file_type)
             tttrs[ff] = tttr    
     return tttrs
 
 def read_analysis(
     paris_path : pathlib.Path,
     paths: List[str] = ['bg4', 'bi4_bur'], #
-    file_endings: List[str] = ['bg4', 'bur']  # 
+    file_endings: List[str] = ['bg4', 'bur'],  # 
+    file_type: str = "PTU"
 ) -> (pd.DataFrame, Dict[str, tttrlib.TTTR]):
     
     info_path = paris_path / 'Info'
@@ -62,5 +59,5 @@ def read_analysis(
             # Handle exceptions, e.g., if the column contains non-numeric values
             print(f"Could not convert {column} to numeric")
     tttrs = dict()
-    update_tttr_dict(df, data_path, tttrs)
+    update_tttr_dict(df, data_path, tttrs, file_type)
     return df, tttrs
