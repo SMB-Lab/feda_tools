9eb1b4662f08cb4f190d52293c5c74d59f979ca6
Alex <korte@clemson.edu>
2024-11-12 13:46:09 -0500

    Migrate to centralized state + one window application

--- feda_tools/gui/fit23_preview.py
+++ feda_tools/gui/fit23_preview.py
@@ -3,20 +3,11 @@ from .widgets import PlotWidget
 from feda_tools.core import model
 import numpy as np
 
-class Fit23PreviewWindow(QtWidgets.QWidget):
-    analysis_started = QtCore.pyqtSignal()
-
-    def __init__(self, data_ptu, data_irf, burst_index, chunk_size, output_directory):
+class Fit23PreviewWidget(QtWidgets.QWidget):
+    def __init__(self, state_handler):
         super().__init__()
-        self.data_ptu = data_ptu
-        self.data_irf = data_irf
-        self.burst_index = burst_index
-        self.chunk_size = chunk_size
-        self.output_directory = output_directory  # Save the output directory
-
+        self.state_handler = state_handler
         self.init_ui()
-        self.thread = None
-        self.worker = None
 
     def init_ui(self):
         self.setWindowTitle('Fit23 Preview')
@@ -32,27 +23,27 @@ class Fit23PreviewWindow(QtWidgets.QWidget):
         self.tau_input = QtWidgets.QDoubleSpinBox()
         self.tau_input.setRange(0.0, 100.0)
         self.tau_input.setDecimals(4)
-        self.tau_input.setValue(3.03)  # Default value
+        self.tau_input.setValue(self.state_handler.config.get('initial_tau', 3.03))
         self.tau_input.valueChanged.connect(self.update_preview)
         
         self.gamma_input = QtWidgets.QDoubleSpinBox()
         self.gamma_input.setRange(0.0, 100.0)
         self.gamma_input.setDecimals(4)
-        self.gamma_input.setValue(0.02)  # Default value
+        self.gamma_input.setValue(self.state_handler.config.get('initial_gamma', 0.02))
         self.gamma_input.valueChanged.connect(self.update_preview)
         
         self.r0_input = QtWidgets.QDoubleSpinBox()
         self.r0_input.setRange(0.0, 100.0)
         self.r0_input.setDecimals(4)
-        self.r0_input.setValue(0.38)  # Default value
+        self.r0_input.setValue(self.state_handler.config.get('initial_r0', 0.38))
         self.r0_input.valueChanged.connect(self.update_preview)
         
         self.rho_input = QtWidgets.QDoubleSpinBox()
         self.rho_input.setRange(0.0, 100.0)
         self.rho_input.setDecimals(4)
-        self.rho_input.setValue(1.64)  # Default value
+        self.rho_input.setValue(self.state_handler.config.get('initial_rho', 1.64))
         self.rho_input.valueChanged.connect(self.update_preview)
-
+        
         params_layout.addRow('Initial Tau:', self.tau_input)
         params_layout.addRow('Initial Gamma:', self.gamma_input)
         params_layout.addRow('Initial r0:', self.r0_input)
@@ -65,36 +56,28 @@ class Fit23PreviewWindow(QtWidgets.QWidget):
         run_fit_button.clicked.connect(self.run_fit23)
         layout.addWidget(run_fit_button)
 
-        # Run Analysis button
-        self.run_button = QtWidgets.QPushButton('Run Full Analysis')
-        layout.addWidget(self.run_button)
-        self.run_button.clicked.connect(self.start_analysis)
+        # Next button
+        self.next_button = QtWidgets.QPushButton('Next')
+        layout.addWidget(self.next_button)
 
         self.setLayout(layout)
         self.plot_fit23(initial_plot=True)
 
     def plot_fit23(self, initial_plot=False):
-        # Prepare data for the entire chunk
-        num_bins = 128
-
-        # Get micro_times from data_ptu
-        all_micro_times = self.data_ptu.micro_times
-
-        # Create histogram counts for the chunk
+        num_bins = self.state_handler.config.get('num_bins', 128)
+        all_micro_times = self.state_handler.data_ptu.micro_times
         counts, _ = np.histogram(all_micro_times, bins=num_bins)
 
-        # Prepare counts_irf from data_irf
-        counts_irf, _ = np.histogram(self.data_irf.micro_times, bins=num_bins)
+        counts_irf, _ = np.histogram(self.state_handler.data_irf.micro_times, bins=num_bins)
         counts_irf_nb = counts_irf.copy()
         counts_irf_nb[0:3] = 0
         counts_irf_nb[10:66] = 0
         counts_irf_nb[74:128] = 0
 
-        # Setup fit23 with default or provided parameters
-        macro_res = self.data_ptu.get_header().macro_time_resolution
-        g_factor = 1.04
-        l1_japan_corr = 0.0308
-        l2_japan_corr = 0.0368
+        macro_res = self.state_handler.data_ptu.get_header().macro_time_resolution
+        g_factor = self.state_handler.config.get('g_factor', 1.04)
+        l1_japan_corr = self.state_handler.config.get('l1_japan_corr', 0.0308)
+        l2_japan_corr = self.state_handler.config.get('l2_japan_corr', 0.0368)
 
         if initial_plot:
             initial_fit_params = np.array([
@@ -112,13 +95,11 @@ class Fit23PreviewWindow(QtWidgets.QWidget):
                 self.rho_input.value()
             ])
 
-
         self.fit23_model = model.setup_fit23(
             num_bins, macro_res, counts_irf_nb, g_factor, l1_japan_corr, l2_japan_corr
         )
 
-        # Perform fitting
-        x0 = self.fit_params  # Use current fit_params
+        x0 = self.fit_params
         fixed = np.array([0, 0, 1, 0])
         try:
             r2 = self.fit23_model(data=counts, initial_values=x0, fixed=fixed, include_model=True)
@@ -127,7 +108,8 @@ class Fit23PreviewWindow(QtWidgets.QWidget):
             QtWidgets.QMessageBox.warning(self, 'Fit23 Error', f'An error occurred during Fit23:\n{e}')
             return
 
-        # Plot data and model using PyQtGraph
+        self.state_handler.fit_params = self.fit_params
+
         self.plot_widget.clear()
         x = np.arange(len(counts))
         self.plot_widget.plot(x, counts / np.max(counts), pen='b', symbol='o', symbolSize=4, name='Data')
@@ -135,26 +117,13 @@ class Fit23PreviewWindow(QtWidgets.QWidget):
         self.plot_widget.plot(x, self.fit23_model.model / np.max(self.fit23_model.model), pen='g', name='Model')
 
         self.plot_widget.setLogMode(y=True)
-        self.plot_widget.setYRange(np.log10(0.001), np.log10(1))  # Convert to log scale
+        self.plot_widget.setYRange(np.log10(0.001), np.log10(1))
         self.plot_widget.setLabel('left', 'log(Counts)')
         self.plot_widget.setLabel('bottom', 'Channel Number')
-        self.plot_widget.addLegend(offset=(30,30))  # Add offset to make legend visible
-    
+        self.plot_widget.addLegend(offset=(30,30))
+
     def update_preview(self):
         self.plot_fit23()
 
     def run_fit23(self):
-        # Re-plot Fit23 with user-defined initial parameters
         self.plot_fit23()
-
-    def start_analysis(self):
-        # Proceed to full analysis using the latest fit_params
-        from .process_analysis import ProcessAnalysisWindow
-
-        self.process_window = ProcessAnalysisWindow(
-            self.data_ptu, self.data_irf, self.burst_index, self.chunk_size, self.fit_params, self.output_directory
-        )
-        self.process_window.show()
-        self.analysis_started.emit()
-        self.close()
-
--- feda_tools/gui/process_analysis.py
+++ feda_tools/gui/process_analysis.py
@@ -1,9 +1,8 @@
-# File: gui/process_analysis.py
-
 from PyQt6 import QtWidgets, QtCore
 from PyQt6.QtCore import QThread, pyqtSignal
 from .widgets import PlotWidget
 from feda_tools.core import process as proc
+from feda_tools.core import analysis as an
 from feda_tools.core import data as dat
 from feda_tools.core import model
 import numpy as np
@@ -11,64 +10,62 @@ import pandas as pd
 
 class Worker(QtCore.QObject):
     progress_update = pyqtSignal(int)
-    data_ready = pyqtSignal(list, list, list, list)  # Signal to send data to the main thread
+    data_ready = pyqtSignal(list, list, list, list)
     finished = pyqtSignal()
     error_occurred = pyqtSignal(str)
 
-    def __init__(self, data_ptu, data_irf, burst_index, chunk_size, fit_params, update_interval):
+    def __init__(self, state_handler, update_interval):
         super().__init__()
-        self.data_ptu = data_ptu
-        self.data_irf = data_irf
-        self.burst_index = burst_index
-        self.chunk_size = chunk_size
-        self.fit_params = fit_params
+        self.state_handler = state_handler
         self.update_interval = update_interval
 
     @QtCore.pyqtSlot()
     def run(self):
         try:
-            # Set initial parameters
-            min_photon_count = 60
-            bg4_micro_time_min = 0
-            bg4_micro_time_max = 12499
-            g_factor = 1.04
-            l1_japan_corr = 0.0308
-            l2_japan_corr = 0.0368
-            bg4_bkg_para = 0
-            bg4_bkg_perp = 0
-            num_bins = 128
+            data_ptu = self.state_handler.data_ptu
+            data_irf = self.state_handler.data_irf
+            burst_index = self.state_handler.burst_index
+            fit_params = self.state_handler.fit_params
 
-            # Prepare IRF data for Fit23
-            counts_irf, _ = np.histogram(self.data_irf.micro_times, bins=num_bins)
+            min_photon_count = self.state_handler.config.get('min_photon_count', 60)
+            bg4_micro_time_min = self.state_handler.config.get('bg4_micro_time_min', 0)
+            bg4_micro_time_max = self.state_handler.config.get('bg4_micro_time_max', 12499)
+            g_factor = self.state_handler.config.get('g_factor', 1.04)
+            l1_japan_corr = self.state_handler.config.get('l1_japan_corr', 0.0308)
+            l2_japan_corr = self.state_handler.config.get('l2_japan_corr', 0.0368)
+            bg4_bkg_para = self.state_handler.config.get('bg4_bkg_para', 0)
+            bg4_bkg_perp = self.state_handler.config.get('bg4_bkg_perp', 0)
+            num_bins = self.state_handler.config.get('num_bins', 128)
+
+            counts_irf, _ = np.histogram(data_irf.micro_times, bins=num_bins)
             counts_irf_nb = counts_irf.copy()
             counts_irf_nb[0:3] = 0
             counts_irf_nb[10:66] = 0
             counts_irf_nb[74:128] = 0
-            macro_res = self.data_ptu.get_header().macro_time_resolution
+            macro_res = data_ptu.get_header().macro_time_resolution
             fit23_model = model.setup_fit23(
                 num_bins, macro_res, counts_irf_nb, g_factor, l1_japan_corr, l2_japan_corr
             )
 
-            # Prepare data containers
             sg_sr = []
             mean_macro_time = []
             tau_values = []
             r_s_values = []
-            total_bursts = len(self.burst_index)
+            total_bursts = len(burst_index)
+            all_bi4_bur_df = []
+            all_bg4_df = []
 
-            # Processing each burst
-            for i, burst in enumerate(self.burst_index, 1):
+            for i, burst in enumerate(burst_index, 1):
                 if len(burst) < min_photon_count:
                     continue
 
-                # Process burst and gather data for plotting
                 bi4_bur_df, bg4_df = proc.process_single_burst(
                     burst,
-                    self.data_ptu.macro_times,
-                    self.data_ptu.micro_times,
-                    self.data_ptu.routing_channels,
+                    data_ptu.macro_times,
+                    data_ptu.micro_times,
+                    data_ptu.routing_channels,
                     macro_res,
-                    self.data_ptu.get_header().micro_time_resolution,
+                    data_ptu.get_header().micro_time_resolution,
                     min_photon_count,
                     bg4_micro_time_min,
                     bg4_micro_time_max,
@@ -78,7 +75,7 @@ class Worker(QtCore.QObject):
                     bg4_bkg_para,
                     bg4_bkg_perp,
                     fit23_model,
-                    self.fit_params
+                    fit_params
                 )
 
                 if bi4_bur_df is not None and bg4_df is not None:
@@ -87,33 +84,35 @@ class Worker(QtCore.QObject):
                     r_s_value = bg4_df['Fit rs_scatter'].values[0]
                     mean_macro = bi4_bur_df['Mean Macro Time (ms)'].values[0]
 
-                    # Append data to lists
                     sg_sr.append(sg_sr_value)
                     mean_macro_time.append(mean_macro)
                     tau_values.append(tau_value)
                     r_s_values.append(r_s_value)
 
-                # Update progress
-                self.progress_update.emit(int(i / total_bursts * 100))
+                    all_bi4_bur_df.append(bi4_bur_df)
+                    all_bg4_df.append(bg4_df)
 
-            # Emit data to the main thread
+                if i % self.update_interval == 0 or i == total_bursts:
+                    self.progress_update.emit(int(i / total_bursts * 100))
+
             self.data_ready.emit(sg_sr, mean_macro_time, tau_values, r_s_values)
+
+            bi4_bur_df_combined = pd.concat(all_bi4_bur_df, ignore_index=True)
+            bg4_df_combined = pd.concat(all_bg4_df, ignore_index=True)
+
+            output_directory = self.state_handler.output_directory
+            file_path = self.state_handler.file_ptu
+            dat.save_results(output_directory, file_path, bi4_bur_df_combined, bg4_df_combined)
+
             self.finished.emit()
         except Exception as e:
             self.error_occurred.emit(str(e))
             self.finished.emit()
 
-class ProcessAnalysisWindow(QtWidgets.QWidget):
-    def __init__(self, data_ptu, data_irf, burst_index, chunk_size, fit_params, output_directory):
+class ProcessAnalysisWidget(QtWidgets.QWidget):
+    def __init__(self, state_handler):
         super().__init__()
-        self.data_ptu = data_ptu
-        self.data_irf = data_irf
-        self.burst_index = burst_index
-        self.chunk_size = chunk_size
-        self.fit_params = fit_params
-        self.output_directory = output_directory
-        self.file_ptu = "output.ptu"  # Placeholder for PTU file path
-
+        self.state_handler = state_handler
         self.init_ui()
         self.thread = None
         self.worker = None
@@ -131,7 +130,7 @@ class ProcessAnalysisWindow(QtWidgets.QWidget):
         interval_label = QtWidgets.QLabel('Update Interval (bursts):')
         self.interval_input = QtWidgets.QSpinBox()
         self.interval_input.setRange(1, 1000)
-        self.interval_input.setValue(10)  # Default update interval
+        self.interval_input.setValue(10)
         interval_layout.addWidget(interval_label)
         interval_layout.addWidget(self.interval_input)
         layout.addLayout(interval_layout)
@@ -162,19 +161,15 @@ class ProcessAnalysisWindow(QtWidgets.QWidget):
         self.start_button.setEnabled(False)
         self.update_interval = self.interval_input.value()
 
-        # Initialize Worker and Thread
+        self.prepare_data()
+
         self.thread = QThread()
         self.worker = Worker(
-            self.data_ptu,
-            self.data_irf,
-            self.burst_index,
-            self.chunk_size,
-            self.fit_params,
+            self.state_handler,
             self.update_interval
         )
         self.worker.moveToThread(self.thread)
 
-        # Connect signals and slots
         self.thread.started.connect(self.worker.run)
         self.worker.progress_update.connect(self.update_progress)
         self.worker.data_ready.connect(self.update_plots)
@@ -184,8 +179,28 @@ class ProcessAnalysisWindow(QtWidgets.QWidget):
         self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
 
-        # Start the thread
         self.thread.start()
+
+    def prepare_data(self):
+        all_macro_times = self.state_handler.data_ptu.macro_times
+        all_micro_times = self.state_handler.data_ptu.micro_times
+
+        macro_res = self.state_handler.data_ptu.get_header().macro_time_resolution
+        micro_res = self.state_handler.data_ptu.get_header().micro_time_resolution
+
+        photon_time_intervals, _ = an.interphoton_arrival_times(
+            all_macro_times, all_micro_times, macro_res, micro_res
+        )
+
+        window_size = self.state_handler.config.get('window_size', 30)
+        running_avg = an.running_average(photon_time_intervals, window_size)
+        logrunavg = np.log10(running_avg)
+
+        threshold_multiplier = self.state_handler.threshold_multiplier or self.state_handler.config.get('threshold_multiplier_default', 4)
+        threshold_value = self.state_handler.mu - threshold_multiplier * self.state_handler.std
+
+        burst_index, _ = dat.extract_greater(logrunavg, threshold_value)
+        self.state_handler.burst_index = burst_index
 
     @QtCore.pyqtSlot(int)
     def update_progress(self, value):
@@ -193,9 +208,6 @@ class ProcessAnalysisWindow(QtWidgets.QWidget):
 
     @QtCore.pyqtSlot(list, list, list, list)
     def update_plots(self, sg_sr, mean_macro_time, tau_values, r_s_values):
-        # Update the plots with data received from the worker
-
-        # sg/sr vs Mean Macro Time
         self.plot_widget1.clear()
         self.plot_widget1.plot(mean_macro_time, sg_sr, pen=None, symbol='o', symbolSize=5)
         self.plot_widget1.setLabel('bottom', 'Mean Macro Time (ms)')
@@ -203,7 +215,6 @@ class ProcessAnalysisWindow(QtWidgets.QWidget):
         self.plot_widget1.setTitle('sg/sr vs Mean Macro Time')
         self.plot_widget1.showGrid(x=True, y=True)
 
-        # sg/sr vs Tau
         self.plot_widget2.clear()
         self.plot_widget2.plot(tau_values, sg_sr, pen=None, symbol='o', symbolSize=5, brush='g')
         self.plot_widget2.setLabel('bottom', 'Tau')
@@ -211,7 +222,6 @@ class ProcessAnalysisWindow(QtWidgets.QWidget):
         self.plot_widget2.setTitle('sg/sr vs Tau')
         self.plot_widget2.showGrid(x=True, y=True)
 
-        # r_s vs Tau
         self.plot_widget3.clear()
         self.plot_widget3.plot(tau_values, r_s_values, pen=None, symbol='o', symbolSize=5, brush='r')
         self.plot_widget3.setLabel('bottom', 'Tau')
